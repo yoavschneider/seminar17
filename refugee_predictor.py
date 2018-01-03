3bed1e6b772d457714acacbc7b7bb97bbfe46cdc
philllip <philllip@phi3.14>
2018-01-03 11:22:04 +0100

    Forecast for more than one month, better plotting

--- refugee_predictor.py
+++ refugee_predictor.py
@@ -32,31 +32,43 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-train_amount = 420
+train_amount = 300
 values_per_country = 444
 
-##train_range = range(0, train_amount)
-#test_range = range(train_amount, train_amount + test_amount)
 
-def save_plot(i, x, y, scaler_x, scaler_y, path):
-    time = arange(2850,3064)
+def save_plot(index, x, y, scaler_x, scaler_y, path, forecast, start, end):
+    x_scaled = scaler_x.transform(x[start:end])
 
-    x_scaled = scaler_x.transform(x)
+    predicted = model.predict(x_scaled)
+    predicted = scaler_y.inverse_transform(predicted)
 
-    plot_data_predicted = model.predict(x_scaled[2850:3064])
-    plot_data_predicted = scaler_y.inverse_transform(plot_data_predicted)
+    predicted_partial = []
+    expected_partial = []
 
-    plot_data_real = y[2850:3064]
+    for i in arange(start, end):
+        if ((i - start) % forecast == 0):
+            predicted_partial.append(predicted[i - start])
+            expected_partial.append(y[i])
+    
+    plot_data_predicted = [num for elem in predicted_partial for num in elem]
+    plot_data_expected = [num for elem in expected_partial for num in elem]
 
-    plt.plot(time, plot_data_real)
+    time = arange(start, end)
     plt.plot(time, plot_data_predicted)
 
+    time = arange(start, end)
+    plt.plot(time, plot_data_expected)
+
+    time = arange(start,end,forecast)
+    plt.plot(time,[plot_data_predicted[i - start] for i in arange(start,end,forecast)], 'ro')
+
     plt.ylabel('refugees')
-    #plt.show()
-    plt.savefig(path + str(i) + '.jpg')
+    # plt.show()
+    plt.savefig(path + str(index) + '.jpg', dpi=1000)
+    print ("Graph saved to " + path + str(index) + '.jpg')
     plt.clf()
 
-def load_data(file, lookback):
+def load_data(file, lookback, forecast):
     x = []
     y = []
     countries = []
@@ -66,7 +78,8 @@ def load_data(file, lookback):
         rows = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in rows:
             country, avg, max, min, disaster, refugees, time = row
-            x.append((int(time), float(avg), float(max), float(min), int(refugees)))
+            x.append((int(time), float(avg), float(
+                max), float(min), int(refugees)))
             y.append((int(time), int(refugees)))
             countries.append(country)
             disasters.append(disaster)
@@ -75,11 +88,12 @@ def load_data(file, lookback):
     disasters = array(disasters)
 
     # Hot one encoding of countries
-    country_encoder ,countries_encoded = unique(countries, return_inverse=True)
-    disaster_encoder, disasters_encoded = unique(disasters, return_inverse=True)
+    country_encoder, countries_encoded = unique(countries, return_inverse=True)
+    disaster_encoder, disasters_encoded = unique(
+        disasters, return_inverse=True)
 
-    x = zip (countries_encoded, disasters_encoded, x)
-    y = zip (countries_encoded, y)
+    x = zip(countries_encoded, disasters_encoded, x)
+    y = zip(countries_encoded, y)
 
     # format now (country, time, avg, max, min, refugees)
     # convert to [country] + [avg,max,min] + lookback * [avg,max,min,refugees]
@@ -91,10 +105,14 @@ def load_data(file, lookback):
     for (country, disaster, (time, avg, max, min, refugees)) in x:
         if (time == 0):
             del last_values[:]
+
+        if (time > values_per_country - forecast):
+            continue
+
         if (lookback == 0):
-            x_final.append([country,avg,max,min,disaster])
+            x_final.append([country, avg, max, min, disaster])
         elif (time >= lookback):
-            x_final.append([country,avg,max,min,disaster] + last_values)
+            x_final.append([country, avg, max, min, disaster] + last_values)
             last_values.pop(0)
             last_values.pop(0)
             last_values.pop(0)
@@ -110,23 +128,43 @@ def load_data(file, lookback):
             last_values.append(max)
             last_values.append(min)
             last_values.append(disaster)
-            last_values.append(refugees)    
+            last_values.append(refugees)
+
+    del last_values[:]
 
     for (country, (time, refugees)) in y:
-        if (time >= lookback):
-            y_final.append(refugees)
+        if (time == 0):
+            if (len(last_values) > 0):
+                y_final.append(last_values.copy())
+            del last_values[:]
 
+        if (time < lookback):
+            continue
+
+    # 0,1,2,3,4,5,6,7,8,9,10,11,12,13
+    #             0,1,2,3,4,5,6,7,8,9
+
+        if (time - lookback > forecast - 1):
+            y_final.append(last_values.copy())
+            last_values.pop(0)
+            last_values.append(refugees)
+        else:
+            last_values.append(refugees)
+
+    y_final.append(last_values.copy())
     return (x_final, y_final)
 
-def split_data(x,y,lookback, train_amount):
+def split_data(x, y, lookback, forecast, train_amount):
     x_train = []
     y_train = []
     x_test = []
     y_test = []
 
+    print(str(len(x)) + "," + str(len(y)))
+
     # First train_amount values are for training (out of 444 months)
-    for i in range(0,len(list(x))):
-        if ((i % 444) < train_amount):
+    for i in range(0, len(list(x))):
+        if (i % (values_per_country - forecast + 1) < train_amount):
             x_train.append(x[i])
             y_train.append(y[i])
         else:
@@ -134,20 +172,25 @@ def split_data(x,y,lookback, train_amount):
             y_test.append(y[i])
 
     x_train = asarray(x_train)
-    y_train = asarray(y_train).reshape(-1,1)
+    y_train = asarray(y_train)
     x_test = asarray(x_test)
-    y_test = asarray(y_test).reshape(-1,1)
+    y_test = asarray(y_test)
 
     return (x_train, y_train, x_test, y_test)
 
 # fix random seed for reproducibility
-#numpy.random.seed(7)
+# numpy.random.seed(7)
 
 lookback = 6
+forecast = 3
+plot_start = 2850
+plot_end = 2904
+number_of_epochs = 10
+steps = 10
 
 # load the dataset
-(x, y) = load_data("ALL_DATA_2_1.csv", lookback)
-x_train, y_train, x_test, y_test = split_data(x, y, lookback, train_amount)
+(x, y) = load_data("ALL_DATA_2_1.csv", lookback, forecast)
+x_train, y_train, x_test, y_test = split_data(x, y, lookback, forecast, train_amount)
 
 # scale values
 scX = StandardScaler()
@@ -155,8 +198,8 @@ scY = MinMaxScaler()
 x_train_scaled = scX.fit_transform(x_train)
 x_test_scaled = scX.transform(x_test)
 
-y_train_scaled = scY.fit_transform(y_train.reshape(-1,1))
-y_test_scaled = scY.transform(y_test.reshape(-1,1))
+y_train_scaled = scY.fit_transform(y_train)
+y_test_scaled = scY.transform(y_test)
 
 input_dimension = 5 + lookback * 5
 
@@ -167,27 +210,31 @@ y_test = y_test_scaled
 
 # create model
 model = Sequential()
-model.add(Dense(units =  4 + lookback * 4, kernel_initializer = 'random_uniform', activation = 'relu', input_dim = input_dimension))
-model.add(Dense(units = 4 + lookback * 3, kernel_initializer = 'random_uniform', activation = 'relu'))
-model.add(Dense(units = 4 + lookback * 2, kernel_initializer = 'random_uniform', activation = 'relu'))
-model.add(Dense(units = 4 + lookback, kernel_initializer = 'random_uniform', activation = 'relu'))
-model.add(Dense(units = 1, kernel_initializer = 'random_uniform', activation = 'relu'))
-model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['mse'])
+model.add(Dense(units=4 + lookback * 4, kernel_initializer='random_uniform',
+                activation='relu', input_dim=input_dimension))
+model.add(Dense(units=4 + lookback * 3,
+                kernel_initializer='random_uniform', activation='relu'))
+model.add(Dense(units=4 + lookback * 2,
+                kernel_initializer='random_uniform', activation='relu'))
+model.add(Dense(units=4 + lookback,
+                kernel_initializer='random_uniform', activation='relu'))
+model.add(Dense(units=forecast, kernel_initializer='random_uniform', activation='relu'))
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mse'])
 
 # PATH
 path = './models/new/model'
 
 # Or load model
-model = load_model(path + '.h5')
+# model = load_model(path + '.h5')
 print(model.summary())
 
-# TRAIN
-for i in range(1,10):    
-    model.fit(x_train, y_train, epochs=100, batch_size=train_amount, verbose=2)
-
-    # Save model
-    model.save(path + '.h5')
-    print("Saved to: " + path + '.h5')
+# Train and plot results
+for i in range(0, steps):
+    model.fit(x_train, y_train, epochs=number_of_epochs, batch_size=train_amount, verbose=2)
 
     # Plot results
-    save_plot(i, x, y, scX, scY, path)
+    save_plot(i, x, y, scX, scY, path, forecast, plot_start, plot_end)
+
+# Save model
+model.save(path + '.h5')
+print("Saved model to: " + path + '.h5')
